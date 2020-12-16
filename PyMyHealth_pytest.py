b7cd6463501f1f7d83b4d95f28f9c50ddec3cbd0
awksedgreep <mark.cotner@gmail.com>
2020-12-15 19:37:47 -0500

    Add pytest test file

---
+++ PyMyHealth_pytest.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+class TestPyMyHealth():
+    def setup(self):
+        1 == 1
+
+    def test_nothing(self):
+        assert 1 == 1
+
+
+#if __name__ == '__main__':
+#    unittest.main()
